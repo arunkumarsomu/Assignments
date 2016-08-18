d4e83fbf98a96b05766905bf7e91e2ba492a9057
Arun <kumar_sundaram82@yahoo.com>
2016-08-18 15:54:54 -0400

    added comments

--- Day-4/RPNCalculator.java
+++ Day-4/RPNCalculator.java
@@ -26,29 +26,33 @@ public class RPNCalculator {
 	   // Main method 
 	   public static void main(String args[]) {
 		   RPNCalculator rpn = new RPNCalculator();
-		   char input[] = {'7','2','+','3','*','3','/','2','-'};
-		   int len = 9;
+		   String input[] = {"120","20","+","10","*","10","/","20","-"};
+		   int len = 9,val=0;
 		   int arith1, arith2, result=0;
 		   
 		   Stack<Integer> st = new Stack<Integer>();
 		// Print the problem    
 		   System.out.printf(" Problem is : ");
-		   for (int z = 0; z < len; z++){
-			   System.out.printf("%c",input[z]);
+		   for (int z = 0; z < input.length; z++){
+			   System.out.printf(" %s ",input[z]);
 			}
 		   System.out.println(" ");
-		// Parse through char array and do arithmetic calculation   
-		   for (int i = 0; i < len; i++){
-			   if (Character.getNumericValue(input[i])  >= 0 ){
-			    int val = Character.getNumericValue(input[i]);    
+		// Parse through String array and do arithmetic calculation   
+		   for (int i = 0; i < input.length; i++){
+			   // Convert each position of string to integer 
+			   if (Character.getNumericValue(input[i].charAt(0)) >= 0 ){
+				   for (int k = 0; k < input[i].length(); k++){
+					    val = 10 * val + Character.getNumericValue(input[i].charAt(k));  
+				   }
 			    rpn.push(st, val);
+			    val = 0;  // reset val for next number calculation
 			   }
 			   else{
 				   arith2 = rpn.pop(st);
 				   arith1 = rpn.pop(st);
 				  
 				// case for doing math functions   
-				   switch(input[i]){
+				   switch(input[i].charAt(0)){
 				   
 				   case '+':
 					   System.out.printf("Add both numbers and  ");
